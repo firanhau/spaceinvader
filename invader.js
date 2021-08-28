2b11827c15d47252a5ebf61a801a1976c69af622
firanhau <www.clark.hau23456@gmail.com>
2021-08-28 21:22:48 +0700

    beta

---
+++ invader.js
@@ -0,0 +1,120 @@
+let mycanvas = document.querySelector('canvas');
+let ctx = mycanvas.getContext('2d');
+let buttonleft = document.getElementById('button-left');
+
+mycanvas.width = 1060;
+mycanvas.height = 1325;
+
+const invader=new Array();
+invader[0] = new Image();
+invader[0].src ="./file/images/red.png";
+invader[1] = new Image();
+invader[1].src ="./file/images/green.png";
+invader[2] = new Image();
+invader[2].src ="./file/images/yellow.png";
+invader[3] = new Image();
+invader[3].src ="./file/images/orange.png";
+invader[4] = new Image();
+invader[4].src ="./file/images/blue.png";
+invader[5] = new Image();
+invader[5].src ="./file/images/pink.png";
+
+//Invader...
+class ainvader{
+    constructor(cX, cY, i){
+        this.i=i,
+        this.cX=cX,
+        this.cY=cY,
+        this.cW=106,
+        this.cH=106,
+        this.l=true
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.drawImage(invader[this.i], 0, 0, 106, 106, this.cX * 106, this.cY * 106, this.cW, this.cH)
+    }
+    update(){
+        this.cX;
+        this.cY;
+    }
+    remove(){
+        this.cW;
+        this.cH;
+        this.l;
+    }
+}
+let a=7;
+let b=3;
+
+let level1 = [
+    [0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],
+    [0,1],[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],
+    [0,2],[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],
+]
+
+// khai bao start...
+let start=new Image();
+start.src='./file/images/startgame.png';
+
+let endgame=new Image();
+endgame.src='./file/images/winlost.png';
+
+//start...
+
+let game='start';
+mycanvas.addEventListener('click', function () {
+    switch(game){
+        case 'start':
+            game='play'
+            console.log('hello')
+            break;
+    }
+})
+
+const screenstart = {    
+    draw: function() {
+        ctx.beginPath();
+        ctx.drawImage(start, 0, 0, 885, 587, 212, 212, 636, 318);
+    }
+}
+
+//end game...
+const win={    
+    draw: function() {
+        ctx.beginPath();
+        ctx.drawImage(endgame, 0, 0, 512, 96, 212, 212, 636, 212);
+    }
+}
+const lost={    
+    draw: function() {
+        ctx.beginPath();
+        ctx.drawImage(endgame, 0, 96, 512, 96, 212, 212, 636, 212);
+    }
+}
+
+let inva = [];
+for(let i=0;i<a*b;i++){
+    inva[i]= new ainvader(level1[i][0], level1[i][1], Math.floor(Math.random() * 6));
+}
+function moveinvader() {
+    setInterval(function(){       
+        if(inva[a-1].cY  % 2 == 0 && game=='play'){
+            for(let i=0;i<a*b;i++){
+                inva[i].cX+=1;            
+            }                
+        }
+        if((inva[a-1].cX==10||inva[0].cX==0 && game=='play')){
+            for(let i=0;i<a*b;i++){
+                inva[i].cY+=1;              
+            }   
+        }
+        if(inva[0].cY % 2 ==1 && game=='play'){
+            for(let i=0;i<a*b;i++){
+                inva[i].cX-=1; 
+            } 
+        }        
+    }, 500);  
+};
+
+moveinvader()
+
---
+++ main.js
@@ -0,0 +1,169 @@
+
+
+//background...
+const bgd = new Image();
+bgd.src ='./file/images/galaxy.png';
+const bg = {
+    sX:0,
+    sY:0,
+    sW:614,
+    sH:335,
+    cX:0,
+    cY:0,
+    cW:1060,
+    cH:530,
+    draw: function(){
+        ctx.beginPath();
+        ctx.drawImage(bgd,this.sX,this.sY,this.sW,this.sH,this.cX,this.cY,this.cW,this.cH);
+        ctx.drawImage(bgd,this.sX,this.sY,this.sW,this.sH,this.cX,this.cY+530,this.cW,this.cH);
+        ctx.drawImage(bgd,this.sX,this.sY,this.sW,this.sH,this.cX,this.cY+1060,this.cW,this.cH);
+    }
+}
+
+//spaceship...
+const ship = new Image();
+ship.src ='./file/images/spaceship.png';
+
+class spaceship{
+    constructor(cX){
+        this.cX=cX
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.drawImage(ship, 0, 0, 621, 608, this.cX, 1150, 106, 130 )
+    }
+    update(){
+        this.cX;
+    }
+}
+let moveship=new spaceship(424);
+
+document.getElementById('button-left').addEventListener("click",function(){
+    if(moveship.cX>=53 && game == 'play'){
+        moveship.cX += -106;
+        fireshoot.l=true;
+    }
+})
+document.getElementById('button-right').addEventListener("click",function(){
+    if(moveship.cX<=901 && game == 'play'){
+        moveship.cX += 106;
+        fireshoot.l=true;
+    }
+    
+})
+
+//shoot...
+const fshoot = new Image();
+fshoot.src = './file/images/shoot.png';
+
+class shoot{
+    constructor(cX){
+        this.cX=cX,
+        this.cY=1125,
+        this.cW=53,
+        this.cH=70,
+        this.l= false
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.drawImage(fshoot, 0, 0, 212, 294, this.cX+26.5, this.cY, this.cW, this.cH)
+    }
+    update(){
+        this.cY;
+        this.cX;
+    }
+    remove(){
+        this.cW;
+        this.cH;
+        this.l;
+    }
+}
+
+let fireshoot = new shoot(424);
+
+function moveshoot() {
+    if(fireshoot.cY>=0 && fireshoot.l==true && game=='play'){
+        fireshoot.cY-= 26.5;
+    }else{
+        fireshoot.cX=moveship.cX;
+        fireshoot.cY=1166;
+        fireshoot.cW=53;
+        fireshoot.cH=70;
+        fireshoot.l=true;
+    }   
+}
+let d=0;
+function removeinvader() {
+    for(let i=0;i<a*b;i++){        
+        if(inva[i].cY*106 == fireshoot.cY && inva[i].cX*106 == fireshoot.cX){               
+            inva[i].cW=0;
+            inva[i].cH=0;
+            if(inva[i].l==true){
+                fireshoot.l=false;d++;
+                document.getElementById('core').innerHTML=d;
+            }
+            inva[i].l=false;                  
+        }
+        
+    }
+}
+let kq='chua biet';
+function winlost(){
+    for(let i=0;i<a*b;i++){
+        if(inva[i].cY * 106 > 1150 && inva[i].l == true && game=='play'){
+            kq='thua';
+            game='end';
+        }
+        if(d==a*b && inva[i].cY * 106 <= 1150 && game=='play'){
+            kq='thang';            
+            game='end';
+        }                
+    }
+    if(kq=='thang'){
+        win.draw();
+    }
+    if(kq=='thua'){
+        lost.draw();
+    }
+}
+
+function remove() {
+    fireshoot.remove();
+    for(let i=0;i<a*b;i++){
+        inva[i].remove();        
+    } 
+       
+}
+function update(){   
+    screenend.update();
+    moveship.update();
+    for(let i=0;i<a*b;i++){
+        inva[i].update();        
+    } 
+    while(true){
+        fireshoot.update();
+    }
+             
+}
+function draw(){
+    bg.draw();
+    moveship.draw(); 
+    for(let i=0;i<a*b;i++){
+        inva[i].draw();
+    }              
+    removeinvader();
+    if(game=='play'){
+        fireshoot.draw();
+        moveshoot();
+    }
+    if(game=='start'){
+        screenstart.draw();
+    }
+    winlost();
+}
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
+    draw();    
+}
+animate();
